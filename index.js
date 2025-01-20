5b696138a3f701d18e1f0a6dd0807a87afcdbe08
Wajeeh uddin <itxahmed927@gmail.com>
2025-01-20 07:02:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var cities = [
+    "Karachi", "Lahore", "Quetta", "Rawalpindi", "Multan", "Faislabad", 
+    "Hyderabad", "Peshawar", "Gujranwala", "Islamabad", "Sialkot", "Sukkur",
+    "Bahawalpur", "Larkana", "Dera Ghazi Khan", "Mingora", "Sahiwal", "Mirpur Khas",
+    "Chiniot", "Okara", "Skardu"
+];
+
+  for (var i = 0; i <= 20; i++ ){
+    if ( cities[i] === "Islamabad"){
+        console.log("Islamabad is the cleanest city of Pakistan")
+        console.log(i)
+    }
+  }
